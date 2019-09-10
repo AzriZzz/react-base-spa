382b762aef0cdbf800eb1303d94107d45cb60115
Azri Zainal <azri.zainal@lionandlion.com>
2019-09-10 21:12:45 +0800

    add header and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React , { Component }from 'react';
+import Footer from './templates/footer';
+import Header from "./templates/header";
 
 class App extends Component{
   render() {
     return (
       <div className="App">
+        <Header />
         <div className="container mt-5 pt-5">
           <h1 className="black">Is this working!</h1>
         </div>
+        <Footer />
       </div>
     )
   }
