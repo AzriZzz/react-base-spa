8ce53ccc6e7140d962cf879991c8fc0b823a26ba
Azri Zainal <azri.zainal@lionandlion.com>
2019-09-11 21:09:29 +0800

    add body js and styling for btn-nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React , { Component }from 'react';
 import Footer from './templates/footer';
 import Header from "./templates/header";
+import Body from './templates/body';
 
 class App extends Component{
   render() {
@@ -8,8 +9,9 @@ class App extends Component{
       <div className="App">
         <Header />
         <div className="container mt-5 pt-5">
-          <h1 className="black">Is this working!</h1>
+          <h1 className="black">This is React</h1>
         </div>
+        <Body />
         <Footer />
       </div>
     )
